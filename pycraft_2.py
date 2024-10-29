32d8a1211aa391574445f4225c43a199bfb40b2e
HumbleBeing5255 <pabschvincent@gmail.com>
2024-10-29 21:37:50 +0100

    Update pycraft_2.py
    updated code
    still missing:
    -correct collision
    -a lot

--- pycraft_2.py
+++ pycraft_2.py
@@ -3,40 +3,41 @@ import pygame as pg
 from collections import deque
 pg.init()
 clock=pg.time.Clock()
-scw,sch,bls=1000,700,30
+scw,sch,bls=1000,700,50
 world_width=100
 xspeed_pl,yspeed_pl,stand,mouse_down,mid_x,mid_y,block_list,block_list_y,xofs,white,black=0,0,True,False,scw//2,sch//2,deque([]),deque([]),0,(255,255,255),(0,0,0)
 font=pg.font.Font(None,50)
-collide_foot=pg.USEREVENT+1
-collide_head=pg.USEREVENT+2
-collide_left=pg.USEREVENT+3
-collide_right=pg.USEREVENT+4
 screen=pg.display.set_mode((scw,sch))
 block_rect=pg.rect.Rect(0,0,bls,bls)
-player_rect=pg.rect.Rect(mid_x,mid_y-bls*2,bls*0.8,bls*1.6)
+player_rect=pg.rect.Rect(mid_x,mid_y-bls*4,bls*0.9,bls*1.8)
 player_surface=pg.transform.scale(pg.image.load("Steve.webp"),(player_rect.width,player_rect.height))
-block_surface=pg.transform.scale(pg.image.load("dirt_block.jpg").convert(),(bls,bls))
+dirt_block_surface=pg.transform.scale(pg.image.load("dirt_block.jpg").convert(),(bls,bls))
+stone_block_surface=pg.transform.scale(pg.image.load("stone_block.jpg").convert(),(bls,bls))
 for h in range(world_width*-1,world_width):
     block_list_y=[]
-    for v in range(-1,(sch//bls)-1):
+    for v in range(-1,(sch//bls)):
         if v*bls>=mid_y:
             block_list_y.append([1,h*bls,v*bls])
         else:
             block_list_y.append([0,h*bls,v*bls])
     block_list.append(block_list_y)
+print(f"World width: {len(block_list)}\nWorld hight: {len(block_list[0])}")
 def block_render():
-    global mouse_down,xofs,yspeed_pl,stand
+    global mouse_down,xofs,yspeed_pl,stand,xofs
     for h in range(len(block_list)):
         if abs(block_list[h][0][1]+xofs)<550:
             for v in range(len(block_list[0])):
+                block_rect.topleft=(block_list[h][v][1]+xofs+mid_x-(bls//2),block_list[h][v][2]+bls)
                 if block_list[h][v][0]!=0:
-                    screen.blit(block_surface,(block_list[h][v][1]+xofs+mid_x-(bls//2),block_list[h][v][2]+bls))
+                    screen.blit(dirt_block_surface,(block_list[h][v][1]+xofs+mid_x-(bls//2),block_list[h][v][2]+bls))
                     if player_rect.colliderect(block_rect):
-                        pass
-
-                        #This is where I would put the collision detection (from bottom, top, left and right)
-
-                block_rect.topleft = (block_list[h][v][1]+xofs+mid_x-(bls//2),block_list[h][v][2]+bls)
+                        if player_rect.bottom>block_rect.top:
+                            #player_rect.y=block_rect.y-player_rect.height+1
+                            yspeed_pl=0
+                            stand=True
+                        if player_rect.left<block_rect.right:
+                            xofs+=2
+                            print("left")
                 if block_rect.collidepoint(pg.mouse.get_pos()):
                     pg.draw.rect(screen,black,block_rect,2)
                     if mouse_down:
@@ -47,22 +48,21 @@ def block_render():
                         mouse_down = False
 def player_render():
     global stand,yspeed_pl,xofs
+    yspeed_pl-=bls*0.0065
     player_rect.y-=yspeed_pl
-    yspeed_pl-=0.
-
-    #This is where I would handle the collisions
-
     keys = pg.key.get_pressed()
     if stand:
-        if keys == pg.K_SPACE:
-            yspeed_pl = 10
+        if keys[pg.K_SPACE]:
+            player_rect.y-=1
+            yspeed_pl = 6
             stand = False
-            print("jump")
+            print("Jump")
     if keys[pg.K_a]:
-        xofs += 5
+        xofs += 2
     if keys[pg.K_d]:
-        xofs -= 5
-    screen.blit(player_surface,player_rect.topleft)
+        xofs -= 2
+    screen.blit(player_surface,player_rect)
+    pg.draw.rect(screen,black,player_rect,2)
 while True:
     start_time=pg.time.get_ticks()
     for event in pg.event.get():
